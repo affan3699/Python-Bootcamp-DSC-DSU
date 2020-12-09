dfd588eb545bf83263fb33da9de725bccd31e7ef
affan3699 <affanmurtaza3@gmail.com>
2020-12-10 04:12:00 +0500

    Updated Q1

--- week_1/Q1.py
+++ week_1/Q1.py
@@ -9,4 +9,12 @@ def printNameDiagonally():
 		else:
 			print("   "*i,name[i])
 
+	print()
+
+	for i in reversed(range(len(name))):
+		if i == 0:
+			print(name[0])
+		else:
+			print("   "*i,name[i])
+
 printNameDiagonally()
